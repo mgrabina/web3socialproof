2d45e28fcac1a5cbf000cfd1ef18da48b9b8db03
Martin Grabina <mgrabina@itba.edu.ar>
2024-12-22 17:03:31 -0300

    refetch

--- apps/admin/lib/context/useUserContext.ts
+++ apps/admin/lib/context/useUserContext.ts
@@ -3,74 +3,77 @@
 import { createSupabaseClientForClientSide } from "@/utils/supabase/client";
 import { Session } from "@supabase/supabase-js";
 import { SelectProtocol, SelectUser } from "@web3socialproof/db";
-import { useState } from "react";
-import { useAsync } from "react-async";
+import { useEffect, useState } from "react";
 
 export const useUserContext = () => {
-  const supabase = createSupabaseClientForClientSide();  
   const [isContextLoading, setIsContextLoading] = useState(true);
   const [session, setSession] = useState<Session | null>(null);
   const [user, setUser] = useState<SelectUser | null>(null);
   const [protocol, setProtocol] = useState<SelectProtocol | null>(null);
 
-  useAsync(async () => {
-    setIsContextLoading(true);
+  useEffect(() => {
+    async function fetch() {
+      const supabase = createSupabaseClientForClientSide();
+      setIsContextLoading(true);
 
-    if (!isContextLoading) {
-      return; // Avoid re-fetching data
-    }
+      if (!isContextLoading) {
+        return; // Avoid re-fetching data
+      }
 
-    const {
-      data: { session: auxSession },
-    } = await supabase.auth.getSession();
+      const {
+        data: { session: auxSession },
+      } = await supabase.auth.getSession();
 
-    console.log("Fetching user data...");
+      console.log("Fetching user data...");
 
-    const {
-      data: { user: supabaseUser },
-    } = await supabase.auth.getUser();
+      const {
+        data: { user: supabaseUser },
+      } = await supabase.auth.getUser();
 
-    if (!auxSession || !supabaseUser?.email) {
+      if (!auxSession || !supabaseUser?.email) {
+        setIsContextLoading(false);
+        return;
+      }
+
+      setSession(auxSession);
+
+      const { data: userData, error } = await supabase
+        .from("users_table")
+        .select()
+        .eq("email", supabaseUser.email)
+        .single();
+
+      if (error) {
+        console.error("Error fetching user:", error);
+        setIsContextLoading(false);
+        return;
+      }
+      setUser(userData);
+
+      if (!userData?.protocol_id) {
+        console.error("No protocol_id found for user:", auxSession?.user.email);
+        setIsContextLoading(false);
+        return;
+      }
+
+      const { data: protocolData, error: protocolError } = await supabase
+        .from("protocol_table")
+        .select()
+        .eq("id", userData?.protocol_id)
+        .single();
+
+      if (protocolError) {
+        console.error("Error fetching protocol:", protocolError);
+        setIsContextLoading(false);
+        return;
+      }
+      setProtocol(protocolData);
+
       setIsContextLoading(false);
-      return;
     }
 
-    setSession(auxSession);
-
-    const { data: userData, error } = await supabase
-      .from("users_table")
-      .select()
-      .eq("email", supabaseUser.email)
-      .single();
-
-    if (error) {
-      console.error("Error fetching user:", error);
-      setIsContextLoading(false);
-      return;
-    }
-    setUser(userData);
-
-    if (!userData?.protocol_id) {
-      console.error("No protocol_id found for user:", auxSession?.user.email);
-      setIsContextLoading(false);
-      return;
-    }
-
-    const { data: protocolData, error: protocolError } = await supabase
-      .from("protocol_table")
-      .select()
-      .eq("id", userData?.protocol_id)
-      .single();
-
-    if (protocolError) {
-      console.error("Error fetching protocol:", protocolError);
-      setIsContextLoading(false);
-      return;
-    }
-    setProtocol(protocolData);
-    
-    setIsContextLoading(false);
-  }, [supabase, isContextLoading]);
+    fetch();
+  }, []); // Only run once
 
   return { isContextLoading, session, user, protocol };
 };
